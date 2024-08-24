89a4bb117565f861a4e4a46c76606902caa60a97
ashbur <18829526908@163.com>
2024-08-24 20:46:48 +0800

    添加基础聊天 api

--- llm-ops-api/internal/handler/app_handler.py
+++ llm-ops-api/internal/handler/app_handler.py
@@ -4,12 +4,36 @@
 @File    :app_handler.py
 @Desc    :
 """
+import os
+
+from flask import request
+from openai import OpenAI
 
 
 class AppHandler:
     """
     应用控制器
     """
+
+    def completion(self):
+        """聊天接口"""
+        # 1. 获取接口中的输入
+        query = request.json.get('query')
+        # 2. 构建 OpenAi 客户端，并发起请求
+        client = OpenAI(
+            api_key=os.environ['MY_API_KEY'],
+            base_url=os.environ['OPENAI_API_BASE']
+        )
+        # 3. 得到请求响应，然后将 OpenAI 的响应传递给前端
+        completion = client.chat.completions.create(
+            model="gpt-4o-mini",
+            messages=[
+                {"role": "system", "content": "你是一个有趣的聊天助手，请根据用户的输入回复对应消息"},
+                {"role": "user", "content": query}
+            ]
+        )
+        content = completion.choices[0].message.content
+        return content
 
     def ping(self):
         return {"ping": "pong"}
--- llm-ops-api/internal/router/router.py
+++ llm-ops-api/internal/router/router.py
@@ -25,5 +25,6 @@ class Router:
         bp = Blueprint("llmOps", __name__, url_prefix="")
         # 2. 将 url 与对应的控制器方法做绑定
         bp.add_url_rule("/ping", view_func=self.app_handler.ping)
+        bp.add_url_rule("/app/completion", methods=["POST"], view_func=self.app_handler.completion)
         # 3. 在应用上去注册蓝图
         app.register_blueprint(bp)
